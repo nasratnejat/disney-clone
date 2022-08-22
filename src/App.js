21a9c725386a08f89f1c3d017e5df4d243f9d463
Nasrat Nejat <85827553+nasratnejat@users.noreply.github.com>
2022-08-23 04:15:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './components/Login'
+import Header from './components/Header'
+import Home from './components/Home'
+import Detail from './components/Detail'
+import Footer from './components/Footer.js'
+
+const App = () => {
+	return (
+		<div>
+			<Router>
+				<Header />
+				<Switch>
+					<Route exact path='/'>
+						<Login />
+					</Route>
+					<Route path='/home'>
+						<Home />
+					</Route>
+					<Route path='/detail/:id'>
+						<Detail />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Footer = () => {
+	return <div>Made By Nasrat Nejat</div>
+}
+
+export default Footer
